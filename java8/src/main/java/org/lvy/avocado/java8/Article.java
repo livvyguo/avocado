1ea4c22aaa1e48f7f052ec19889864f031536fbd
guozheng <guozheng@youku.com>
2016-01-25 10:14:14 +0800

    demo

--- java8/src/main/java/org/lvy/avocado/java8/Article.java
+++ java8/src/main/java/org/lvy/avocado/java8/Article.java
@@ -1,7 +1,57 @@
 package org.lvy.avocado.java8;
 
+import java.util.Date;
+
 /**
  * Created by livvy (livvyguo@gmail.com) on 16/1/22.
  */
 public class Article {
+
+    private Integer id;
+    private String authorName;
+    private String title;
+    private Date createDate;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
 }
--- java8/src/test/java/org/lvy/avocado/java8/MainTest.java
+++ java8/src/test/java/org/lvy/avocado/java8/MainTest.java
@@ -1,7 +1,25 @@
 package org.lvy.avocado.java8;
 
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by livvy (livvyguo@gmail.com) on 16/1/22.
  */
 public class MainTest {
+
+
+    @Test
+    public void testJoin() {
+        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Integer integer = integers.stream().map((x) -> x * x).reduce((x, y) -> {
+            int z = y - x;
+            return z * (x + y);
+        }).get();
+
+        System.out.println(integer);
+
+    }
 }
